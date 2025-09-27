db7483fdadad6ebe3ad5592150aa29728a31be2a
xHozey <hamzasaliheddine@hotmail.com>
2025-09-27 11:21:05 +0100

    feat: login/register components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "login", component: Login},
+    {path: "register", component: Register}
+];
