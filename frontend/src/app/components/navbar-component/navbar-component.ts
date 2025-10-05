74c60caf791f7967cf5888f8e08ba0a05f244f6c
xHozey <hamzasaliheddine@hotmail.com>
2025-10-05 12:13:34 +0100

    ui enchanced

--- frontend/src/app/components/navbar-component/navbar-component.ts
+++ frontend/src/app/components/navbar-component/navbar-component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { Bell, LucideAngularModule } from 'lucide-angular';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar-component',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './navbar-component.html',
   styleUrls: ['./navbar-component.css'], 
 })
 export class NavbarComponent {
   isCreatePostPage = false; 
-
+  readonly BellIcon = Bell
   constructor(private router: Router) {
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
